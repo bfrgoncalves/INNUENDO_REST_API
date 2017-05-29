07d64aa8a59c21e46d05ee9be043abd022ecd4e0
Bruno Goncalves <bfgoncalves@medicina.ulisboa.pt>
2017-05-29 17:38:02 +0100

    report changes

--- app/static/controllers/reports_controller.js
+++ app/static/controllers/reports_controller.js
@@ -442,9 +442,9 @@ innuendoApp.controller("reportsCtrl", function($scope, $rootScope, $http) {
 								$.fn.dataTable.tables( { visible: true, api: true } ).columns.adjust(); 
 								console.log(run_infos, reports_info_col_defs, reports_info_table_headers);
 								objects_utils.loadDataTables('reports_info_table', run_infos, reports_info_col_defs, reports_info_table_headers);
-								$('#reports_info_table').css({'display':'block'});
-								$('#reports_results_table').css({'display':'none'});
-								$('#reports_metadata_table').css({'display':'none'});
+								$('#reports_info_table_wrapper').css({'display':'block'});
+								$('#reports_results_table_wrapper').css({'display':'none'});
+								$('#reports_metadata_table_wrapper').css({'display':'none'});
 							});
 						});
 				    }
@@ -454,9 +454,9 @@ innuendoApp.controller("reportsCtrl", function($scope, $rootScope, $http) {
 						$('#reports_container').css({display:"block"});
 						$.fn.dataTable.tables( { visible: true, api: true } ).columns.adjust(); 
 						console.log(run_infos, reports_info_col_defs, reports_info_table_headers);
-						$('#reports_info_table').css({'display':'block'});
-						$('#reports_results_table').css({'display':'none'});
-						$('#reports_metadata_table').css({'display':'none'});
+						$('#reports_info_table_wrapper').css({'display':'block'});
+						$('#reports_results_table_wrapper').css({'display':'none'});
+						$('#reports_metadata_table_wrapper').css({'display':'none'});
 						//table = $('#saved_reports_table').DataTable();
 						//table.draw();
 				    }

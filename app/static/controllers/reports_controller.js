1b3367de8429c7c8b3653e17d9e8b69500039954
Bruno Goncalves <bfgoncalves@medicina.ulisboa.pt>
2017-05-29 16:45:39 +0100

    report changes

--- app/static/controllers/reports_controller.js
+++ app/static/controllers/reports_controller.js
@@ -458,6 +458,23 @@ innuendoApp.controller("reportsCtrl", function($scope, $rootScope, $http) {
 							});
 						});
 				    }
+				    else{
+				    	objects_utils.loadDataTables('reports_info_table', [], reports_info_col_defs, reports_info_table_headers);
+						objects_utils.loadDataTables('reports_results_table', [], reports_info_col_defs, reports_info_table_headers);
+						objects_utils.loadDataTables('reports_metadata_table', [], reports_metadata_col_defs, reports_metadata_table_headers);
+
+						$('#reports_info_table_wrapper').css({'display':'none'});
+						$('#reports_results_table_wrapper').css({'display':'none'});
+						$('#reports_metadata_table_wrapper').css({'display':'none'});
+
+						$('#waiting_spinner').css({display:'none'});
+						$('#reports_container').css({display:"block"});
+
+						table = $('#reports_table').DataTable();
+						table.draw();
+						table = $('#saved_reports_table').DataTable();
+						table.draw();
+				    }
 				});
 		    });
 	    });

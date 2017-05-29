9a5da563fdeebcaa4561b178374779fedab4cf84
Bruno Goncalves <bfgoncalves@medicina.ulisboa.pt>
2017-05-29 13:00:17 +0100

    report changes

--- app/static/controllers/reports_controller.js
+++ app/static/controllers/reports_controller.js
@@ -599,7 +599,7 @@ innuendoApp.controller("reportsCtrl", function($scope, $rootScope, $http) {
 
 			for(job in response.data){
 				//console.log(response.data[job].report_data);
-				if(procedure_name == "INNUca") identifier = Object.keys(response.data[job].report_data.run_info)[0];
+				if(response.data[job].procedure_name == "INNUca") identifier = Object.keys(response.data[job].report_data.run_info)[0];
 				else identifier = "";
 
 				//if(identifier == "stats") response.data[job].report_data.shift();

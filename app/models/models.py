ee01cea10fc81da40465e09f89b6d50e574b99c6
Bruno Goncalves <bfgoncalves@medicina.ulisboa.pt>
2017-05-30 13:48:38 +0100

    report changes

--- app/models/models.py
+++ app/models/models.py
@@ -32,7 +32,7 @@ class User(db.Model, UserMixin):
 	def try_login(email, password):
 		conn = get_ldap_connection()
 		try:
-			conn.simple_bind_s("cn="+email, "dc=innuendo","dc=com", password)
+			conn.simple_bind_s("cn="+email+",dc=innuendo,dc=com", password)
 		except Exception as e:
 			print "BAH"
 			print e

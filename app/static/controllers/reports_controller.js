43914d1d3bd322c11c4f3c98773feaaa0c121b5a
Bruno Goncalves <bfgoncalves@medicina.ulisboa.pt>
2017-06-02 17:19:18 +0100

    send to phyloviz

--- app/static/controllers/reports_controller.js
+++ app/static/controllers/reports_controller.js
@@ -319,10 +319,10 @@ innuendoApp.controller("reportsCtrl", function($scope, $rootScope, $http) {
 
 		//$('#reports_info_table thead').css({'visibility':'hidden'});
 		//$('#reports_info_table tfoot').css({'visibility':'hidden'});
-		/*$('#reports_results_table thead').css({'visibility':'hidden'});
+		$('#reports_results_table thead').css({'visibility':'hidden'});
 		$('#reports_results_table tfoot').css({'visibility':'hidden'});
 		$('#reports_metadata_table thead').css({'visibility':'hidden'});
-		$('#reports_metadata_table tfoot').css({'visibility':'hidden'});*/
+		$('#reports_metadata_table tfoot').css({'visibility':'hidden'});
 
 		objects_utils.destroyTable('reports_info_table');
 	    objects_utils.destroyTable('reports_results_table');
@@ -490,7 +490,7 @@ innuendoApp.controller("reportsCtrl", function($scope, $rootScope, $http) {
 								objects_utils.loadDataTables('reports_info_table', run_infos, reports_info_col_defs, reports_info_table_headers);
 								objects_utils.loadDataTables('reports_metadata_table', current_strains_data, reports_metadata_col_defs, reports_metadata_table_headers);
 								objects_utils.loadDataTables('reports_table', user_reports, user_reports_col_defs, user_reports_table_headers);
-								$('#reports_metadata_table_wrapper').css({'display':'none'});
+								//$('#reports_metadata_table_wrapper').css({'display':'none'});
 								$("#act_rep").trigger("click");
 							});
 						});
@@ -504,7 +504,7 @@ innuendoApp.controller("reportsCtrl", function($scope, $rootScope, $http) {
 						objects_utils.loadDataTables('reports_info_table', run_infos, reports_info_col_defs, reports_info_table_headers);
 						objects_utils.loadDataTables('reports_metadata_table', current_strains_data, reports_metadata_col_defs, reports_metadata_table_headers);
 						objects_utils.loadDataTables('reports_table', [], user_reports_col_defs, user_reports_table_headers);
-						$('#reports_metadata_table_wrapper').css({'display':'none'});
+						//$('#reports_metadata_table_wrapper').css({'display':'none'});
 						$("#act_rep").trigger("click");
 						//table = $('#saved_reports_table').DataTable();
 						//table.draw();

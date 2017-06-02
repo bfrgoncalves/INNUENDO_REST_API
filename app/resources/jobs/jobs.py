162b99f8d626e6aacb478ff58445224520fe92cf
Bruno Goncalves <bfgoncalves@medicina.ulisboa.pt>
2017-06-02 17:41:01 +0100

    send to phyloviz

--- app/resources/jobs/jobs.py
+++ app/resources/jobs/jobs.py
@@ -64,8 +64,9 @@ def add_data_to_db(job_id, results, user_id, procedure,sample, pipeline_id, proc
 		return True, job_id
 	else:
 		if report.job_id == job_id:
-			report.report_data=results
-			db.session.commit()
+			if len(Object.keys(results)) != 0:
+				report.report_data=results
+				db.session.commit()
 			return False, job_id
 		else:
 			print "#####################################################################################################################################"

a952a62981814e5ddb6429e3fdb102043b632767
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-06-20 13:39:09 +0100

    test_add_strain_notification

---
+++ app/static/controllers/overview_controller.js
@@ -0,0 +1,29 @@
+
+innuendoApp.controller("overviewCtrl", function($scope, $rootScope, $http) {
+
+	$scope.showSpeciesDrop = function(){
+
+		var projects_table = new Projects_Table(0, null, $http);
+
+		projects_table.get_species_names(function(results){
+        	$scope.species = results.species;
+	        CURRENT_SPECIES_NAME = results.CURRENT_SPECIES_NAME;
+	        CURRENT_SPECIES_ID = results.CURRENT_SPECIES_ID;
+
+	        console.log(CURRENT_SPECIES_NAME, CURRENT_SPECIES_ID);
+
+	        var t_use = "";
+			for(r in results.species){
+				t_use += '<option>' + results.species[r] + '</option>';
+			}
+			$('#species_select_drop').append(t_use);
+
+			$('.selectpicker').selectpicker({
+			});
+
+
+        });
+
+	}
+
+});

9dbc1a8fc97eb00911b1802162b289d5eee68329
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-07-01 00:17:33 +0100

    add mlst_database and chewbbaca files

--- app/app_configuration.py
+++ app/app_configuration.py
@@ -17,14 +17,14 @@ def before_first_request():
     db.create_all()
 
     # Create the Roles "admin" and "end-user" -- unless they already exist
-    user_datastore.find_or_create_role(name='admin', description='Administrator')
-    user_datastore.find_or_create_role(name='end-user', description='End user')
+    #user_datastore.find_or_create_role(name='admin', description='Administrator')
+    #user_datastore.find_or_create_role(name='end-user', description='End user')
 
     # Create two Users for testing purposes -- unless they already exists.
     # In each case, use Flask-Security utility function to encrypt the password.
-    encrypted_password = utils.encrypt_password(app.config['ADMIN_PASS'])
-    if not user_datastore.get_user(app.config['ADMIN_EMAIL']):
-        user_datastore.create_user(email=app.config['ADMIN_EMAIL'], password=encrypted_password, username=app.config['ADMIN_USERNAME'], name=app.config['ADMIN_NAME'])
+    #encrypted_password = utils.encrypt_password(app.config['ADMIN_PASS'])
+    #if not user_datastore.get_user(app.config['ADMIN_EMAIL']):
+        #user_datastore.create_user(email=app.config['ADMIN_EMAIL'], password=encrypted_password, username=app.config['ADMIN_USERNAME'], name=app.config['ADMIN_NAME'])
 
     # Commit any database changes; the User and Roles must exist before we can add a Role to the User
     specie1 = Specie(name="Campylobacter")
@@ -45,8 +45,8 @@ def before_first_request():
 
     # Give one User has the "end-user" role, while the other has the "admin" role. (This will have no effect if the
     # Users already have these Roles.) Again, commit any database changes.
-    user_datastore.add_role_to_user(app.config['ADMIN_EMAIL'], 'admin')
-    db.session.commit()
+    #user_datastore.add_role_to_user(app.config['ADMIN_EMAIL'], 'admin')
+    #db.session.commit()
 
 
 @app.login_manager.request_loader

4fa94f148c52d9a9766d97b0f8f1f540064aebc6
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-06-30 14:48:52 +0100

    add mlst_database

--- app/utils/mlst_profiles_to_db.py
+++ app/utils/mlst_profiles_to_db.py
@@ -1,4 +1,15 @@
 from app import app, db
 from app.models.models import Ecoli, Yersinia, Campylobacter, Salmonella, Core_Schemas
+import datetime
 
-print "AQUI"
+def populate_db(name, classifier, allelic_profile):
+
+	ecoli = Ecoli(name = name, classifier = classifier, allelic_profile = allelic_profile, timestamp = datetime.datetime.utcnow())
+	db.session.add(ecoli)
+	db.session.commit()
+
+	return 201
+
+allelic_profile = {'coli1':{'loci':'1'}}
+
+populate_db("coli1", "ST3", allelic_profile)

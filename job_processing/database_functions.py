26c6d2724efd93118e0779662c1c4108d9dc3a29
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-06-30 14:52:35 +0100

    add mlst_database

--- job_processing/database_functions.py
+++ job_processing/database_functions.py
@@ -2,9 +2,12 @@ from app import app, db
 from app.models.models import Ecoli, Yersinia, Campylobacter, Salmonella, Core_Schemas
 
 def search_on_database(strain_id, closest_number):
+	return True
 
 
 def add_to_database(strain_id, profile_object, classifier):
+	return True
 
 
 def classify_profile(strain_id, profile_object):
+	return True

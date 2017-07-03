328ea46730fd1de9cd1738e9ddbe07bea8fce78a
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-07-03 13:42:50 +0100

    trees tab on reports. PHYLOViZ On queue

--- job_processing/fast_mlst_functions.py
+++ job_processing/fast_mlst_functions.py
@@ -12,18 +12,16 @@ from subprocess import call
 def get_closest_profiles(profile_query_file_path, index_path, max_closest):
 
 	file_name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
-	result_path = profile_query_file_path + ".result"
+	#result_path = profile_query_file_path + ".result"
 
 
-	command = './dependencies/fast-mlst/src/main -i '+index_path+' -q '+max_closest+' < '+profile_query_file_path+' &> ' + result_path;
+	command = './dependencies/fast-mlst/src/main -i '+index_path+' -q '+max_closest;
 	#command = './dependencies/fast-mlst/src/main --help';
 	#command = 'python ./app/resources/phyloviz/remoteUpload.py -u innuendo_demo -p innuendo_demo -sdt profile -sd ' + file_path_profile + ' -d ' + args.dataset_name + ' -dn ' + args.dataset_description + '-l';
 	command = command.split(' ')
 	print command
 
-	#proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	#stdout, stderr = proc.communicate()
+	proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+	stdout, stderr = proc.communicate(input=profile_query_file_path)
 
-	call(command)
-
-	print "DONE"
+	print stdout, stderr

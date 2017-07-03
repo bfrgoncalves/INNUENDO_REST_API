394d230d566ff5bdda69f79f883a9ba27ec814a2
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-07-03 13:12:38 +0100

    trees tab on reports. PHYLOViZ On queue

--- job_processing/fast_mlst_functions.py
+++ job_processing/fast_mlst_functions.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from app import db
 import random
 import os
@@ -12,7 +10,7 @@ import subprocess
 def get_closest_profiles(profile_query_file_path, index_path, max_closest):
 
 
-	command = 'python ./dependencies/fast-mlst/src/main -i '+index_path+' -q '+max_closest+' < '+profile_query_file_path;
+	command = './dependencies/fast-mlst/src/main -i '+index_path+' -q '+max_closest+' < '+profile_query_file_path;
 	#command = 'python ./app/resources/phyloviz/remoteUpload.py -u innuendo_demo -p innuendo_demo -sdt profile -sd ' + file_path_profile + ' -d ' + args.dataset_name + ' -dn ' + args.dataset_description + '-l';
 	command = command.split(' ')
 

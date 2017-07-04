8851d68ce7c09805de60a8958d2a43ad42dd0567
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-07-04 16:56:46 +0100

    warning color on innuca

--- app/resources/ngs_onto/ngs_onto_processes.py
+++ app/resources/ngs_onto/ngs_onto_processes.py
@@ -243,12 +243,13 @@ class NGSOnto_ProcessListPipelineResource(Resource):
 			newProcType=listOrderedProcessTypes[numberOfProcesses]
 
 			# get specific process input type and uri
-			queryString ="""SELECT DISTINCT (STR(?in) as ?messageURI) WHERE { <"""+newProcType+"""> rdfs:subClassOf ?B. ?B owl:onProperty <http://purl.obolibrary.org/obo/RO_0002234>; owl:someValuesFrom ?outType. <"""+localNSpace+"projects/"+str(id)+"/pipelines/"+str(ppipid)+"""> obo:BFO_0000051  ?proc. <"""+localNSpace+"projects/"+str(id)+"/pipelines/"+str(rpipid)+"""> obo:BFO_0000051  ?proc2. { ?proc obo:RO_0002233 ?in. ?in a ?outType. } UNION { ?proc obo:RO_0002234 ?in. ?in a ?outType. } UNION { ?proc2 obo:RO_0002234 ?in. ?in a ?outType. } UNION { ?proc2 obo:RO_0002234 ?in. ?in a ?outType. } }"""
+			queryString ="""SELECT DISTINCT (STR(?in) as ?messageURI) WHERE { <"""+newProcType+"""> rdfs:subClassOf ?B. ?B owl:onProperty <http://purl.obolibrary.org/obo/RO_0002233>; owl:someValuesFrom ?outType. <"""+localNSpace+"projects/"+str(id)+"/pipelines/"+str(ppipid)+"""> obo:BFO_0000051  ?proc. <"""+localNSpace+"projects/"+str(id)+"/pipelines/"+str(rpipid)+"""> obo:BFO_0000051  ?proc2. { ?proc obo:RO_0002233 ?in. ?in a ?outType. } UNION { ?proc obo:RO_0002234 ?in. ?in a ?outType. } UNION { ?proc2 obo:RO_0002234 ?in. ?in a ?outType. } UNION { ?proc2 obo:RO_0002234 ?in. ?in a ?outType. } }"""
 			print queryString
 			tupleQuery = dbconAg.prepareTupleQuery(QueryLanguage.SPARQL, queryString)
-			result3 = tupleQuery.evaluate()
-			jsonResult2=parseAgraphQueryRes(result3,["messageURI"])
-			result3.close()
+			result4 = tupleQuery.evaluate()
+			print result4
+			jsonResult2=parseAgraphQueryRes(result4,["messageURI"])
+			result4.close()
 			
 			print '#####PASSOU POR ALI###########'
 			print jsonResult2

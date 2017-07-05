69d4c21aab317f3ead65721dbb44256571b8ea48
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-07-05 11:35:34 +0100

    inteligent workflow input-output management

--- app/static/controllers/js_objects/single_project.js
+++ app/static/controllers/js_objects/single_project.js
@@ -1601,6 +1601,7 @@ function Single_Project(CURRENT_PROJECT_ID, CURRENT_PROJECT, $http, $rootScope){
 								}
 								for(y in pipelines_type_by_strain[strain_names[index]][1]){
 									if(pipelines_type_by_strain[strain_names[index]][1][y].indexOf(class_n) < 0){
+										console.log("AQUI", class_n)
 										new_pipapplied_proc.push(pipelines_type_by_strain[strain_names[index]][1][y]);
 									}
 									

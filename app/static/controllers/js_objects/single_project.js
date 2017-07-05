e91a66fbe278fd7316131ea463367c85138ff7d1
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-07-05 20:52:51 +0100

    delete only last workflow

--- app/static/controllers/js_objects/single_project.js
+++ app/static/controllers/js_objects/single_project.js
@@ -934,7 +934,7 @@ function Single_Project(CURRENT_PROJECT_ID, CURRENT_PROJECT, $http, $rootScope){
 		        	//ALLOW ONLY THE LAST WORKFLOW TO BE REMOVED
 		        	console.log("."+proc_value+'&&'+strain_data[counter]['strainID'].replace(/ /g, '_')+"_"+String(pip_start_id)+ '_' + CURRENT_PROJECT_ID+'&&&');
 		        	class_of_button_remove = proc_value+'&&'+strain_data[counter]['strainID'].replace(/ /g, '_')+"_"+String(pip_start_id)+ '_' + CURRENT_PROJECT_ID+'&&&';
-		        	if(pip_start_id > 1) $("."+class_of_button_remove).css({"display":"none"});
+		        	if(pip_start_id > 0) $("."+class_of_button_remove).css({"display":"none"});
 
 		        	
 	        		buttonselectedPipeline = '<div class="dropdown" style="float:left;">'+

cbd1b908636cb89fed29105b96068c437fb82852
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-07-05 22:50:46 +0100

    delete only last workflow

--- app/static/controllers/single_project_controller.js
+++ app/static/controllers/single_project_controller.js
@@ -459,7 +459,7 @@ innuendoApp.controller("projectCtrl", function($scope, $rootScope, $http) {
 		single_project.remove_strains_from_project(global_strains, function(strains_results){
 			objects_utils.destroyTable('strains_table');
 			global_strains = strains_results.strains;
-			objects_utils.loadDataTables('strains_table', global_strains, project_col_defs, sh);
+			objects_utils.loadDataTables('strains_table', global_strains, project_col_defs, strains_headers);
 			//modalAlert("Strains were removed from the project.", function(){});
 		});
 	}
@@ -494,7 +494,7 @@ removeAnalysis = function(li){
 				global_strains[i] = strain_results.strains[i];
 			}
 			objects_utils.destroyTable('strains_table');
-			objects_utils.loadDataTables('strains_table', global_strains, project_col_defs, strains_headers);
+			objects_utils.loadDataTables('strains_table', global_strains, project_col_defs, sh);
 		});
 	}
 

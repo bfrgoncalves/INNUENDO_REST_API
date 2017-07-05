2db601cc19973ae1f731f84f118a374002eac80e
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-07-05 21:24:50 +0100

    delete only last workflow

--- app/static/controllers/js_objects/single_project.js
+++ app/static/controllers/js_objects/single_project.js
@@ -938,9 +938,8 @@ function Single_Project(CURRENT_PROJECT_ID, CURRENT_PROJECT, $http, $rootScope){
 		        	console.log(class_of_button_remove_to_replace);
 		        	if(pip_start_id > 0){
 		        		pipelines_type_by_strain[strain_data[counter-1]['strainID']][1] = pipelines_type_by_strain[strain_data[counter-1]['strainID']][1].replace(class_of_button_remove_to_replace, 'style="display:none;" ' + class_of_button_remove_to_replace)
+		        		console.log(pipelines_type_by_strain[strain_data[counter-1]['strainID']][1]);
 		        	}
-		        	
-		        	console.log(pipelines_type_by_strain[strain_data[counter-1]['strainID']][1]);
 
 		        	
 	        		buttonselectedPipeline = '<div class="dropdown" style="float:left;">'+

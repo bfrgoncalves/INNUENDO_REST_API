c83cdad78db55e699de33f5a7e373fba65bc3ea6
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-07-06 15:41:41 +0100

    fix phyloviz bug

--- job_processing/phyloviz_functions.py
+++ job_processing/phyloviz_functions.py
@@ -180,7 +180,7 @@ def send_to_phyloviz(job_ids, dataset_name, dataset_description, additional_data
 					else:
 						string_metadata.append(strain_metadata[x])
 				except Exception as e:
-					string_metadata.append("")
+					string_metadata.append("undefined")
 
 			print "METADATA",len(string_metadata)
 			all_metadata.append('\t'.join(string_metadata))

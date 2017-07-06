c3cb342c3a64c0b3228e8663ba1ece7300a52d24
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-07-06 12:32:48 +0100

    test

--- app/static/controllers/js_objects/single_project.js
+++ app/static/controllers/js_objects/single_project.js
@@ -1405,15 +1405,15 @@ function Single_Project(CURRENT_PROJECT_ID, CURRENT_PROJECT, $http, $rootScope){
 						    	var s_name = strain_data[x]['strainID'];
 						    	//console.log(pipelines_applied[s_name]);
 						    	//console.log(pipelines_applied, pipelines_applied[s_name]);
-						    	console.log(pipelines_applied);
+						    	//console.log(pipelines_applied);
 						    	for(j in pipelines_applied[s_name]){
 						    			//console.log(pipelines_applied[s_name][j], s_name, pipelines_applied);
 						    			pipeline_id = pipelines_applied[s_name][j].split('id="')[1].split('"')[0];
-						    			console.log(pipeline_id);
-						    			console.log(buttons_to_tasks);
 						    			//console.log(pipeline_id);
 						    			//console.log(buttons_to_tasks);
-						    			if(buttons_to_tasks[pipeline_id].indexOf("null")>-1){
+						    			//console.log(pipeline_id);
+						    			//console.log(buttons_to_tasks);
+						    			if(buttons_to_tasks[pipeline_id] != undefined && buttons_to_tasks[pipeline_id].indexOf("null")>-1){
 						    				pipelines_type_by_strain[s_name][0].push(pipelines_applied[s_name][j].replace("&&&", "&&protocol"));
 						    				toAdd_lab_protocols += pipelines_applied[s_name][j].replace("&&&", "&&protocol");
 						    			}

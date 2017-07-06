2873bfa70cb8a2e9004401cab26a54433265ca87
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-07-06 16:38:01 +0100

    fix phyloviz number of core bug

--- app/resources/phyloviz/remoteUpload.py
+++ app/resources/phyloviz/remoteUpload.py
@@ -8,13 +8,15 @@ import sys
 from datetime import datetime
 import json
 from StringIO import StringIO
+import random, string
 
 
 def main():
 
 	parser = argparse.ArgumentParser(description="This program performs remote upload of data sets to the PHYLOViZ Online application")
-	parser.add_argument('-u', nargs='?', type=str, help="username", required=True)
-	parser.add_argument('-p', nargs='?', type=str, help="password", required=True)
+	parser.add_argument('-u', nargs='?', type=str, help="username", required=False)
+	parser.add_argument('-p', nargs='?', type=str, help="password", required=False)
+	parser.add_argument('-t', nargs='?', type=str, help="user cookie", required=False)
 	parser.add_argument('-e', nargs='?', type=bool, help="Make public", required=False, default=False)
 	parser.add_argument('-l', nargs='?', type=bool, help="Public Link", required=False, default=False)
 	parser.add_argument('-sdt', nargs='?', type=str, help='Sequence data type (newick, profile, fasta)', required=True)
@@ -22,28 +24,56 @@ def main():
 	parser.add_argument('-m', nargs='?', type=str, help="Metadata", required=False)
 	parser.add_argument('-d', nargs='?', type=str, help="Dataset name", required=True)
 	parser.add_argument('-dn', nargs='?', type=str, help="Description", required=False)
-	parser.add_argument('--missing_data', nargs='?', type=str, help="Has missing data", required=False, default="true")
-	parser.add_argument('--missing_char', nargs='?', type=str, help="Missing character", required=False, default="0")
+	parser.add_argument('-root', nargs='?', type=str, help="root of the application", required=False, default="https://online.phyloviz.net")
+	parser.add_argument('-cd', nargs='?', type=str, help="Cookie domain", required=False, default="https://online.phyloviz.net")
+	parser.add_argument('-mc', nargs='?', type=str, help="Missings Character (defaults to None)", required=False, default=False)
+	parser.add_argument('-mt', nargs='?', type=str, help="Missing threshold (defaults to None)", required=False, default=False)
+	parser.add_argument('-pid', nargs='?', type=str, help="Parent dataset identifier (Defaults to None)", required=False, default=False)
+	parser.add_argument('-am', nargs='?', type=str, help="Analysis Method in case of Profile Data (defaults to core) options: core; pres-abs (presence/absence)", required=False, default='core')
 
 	args = parser.parse_args()
 
-	currentRoot = 'http://137.205.70.78'
+	currentRoot = args.root
+	outRoot = args.cd
+
+	def randomword(length):
+	   return ''.join(random.choice(string.lowercase) for i in range(length))
+	
+
+	onqueue = 'false'
+
+	if (not args.u or not args.u) and not args.t:
+		print 'message:' + 'No credentials'
+		sys.exit()
 
 	checkDatasets(args, currentRoot)
 	dataset = remoteUpload(args, currentRoot)
 	if not "datasetID" in dataset:
 		sys.exit()
-	rungoeBURST(args, dataset['datasetID'], currentRoot)
+
+	if dataset["numberOfProfiles"] > 300 or dataset["profileLength"] > 100:
+		onqueue = 'true'
+	
+	goe_message = rungoeBURST(args, dataset['datasetID'], currentRoot, onqueue)
 
 	if not args.e and args.l:
 		sharableLink = generatePublicLink(args, dataset['datasetID'], currentRoot)
 		print 'Sharable link: ' + sharableLink['url']
 		sys.exit()
 
-	if args.e:
-		print "\nAccess the tree at: " + currentRoot + '/main/dataset/' + dataset['datasetID']
-	else:
-		print "\nLogin to PHYLOViZ Online and access the tree at: " + currentRoot + '/main/dataset/' + dataset['datasetID']
+
+	print 'datasetID:' + str(dataset['datasetID'])
+	
+	if 'queue' in goe_message:
+		print 'message:' + goe_message['queue'] + '\n'
+		print 'code:queue'
+		print 'jobid:' + str(goe_message['jobid'])
+	elif args.e:
+		print 'code:complete'
+		print 'message:' + "\nAccess the tree at: " + outRoot + '/main/dataset/' + dataset['datasetID']
+	elif not args.e:
+		print 'code:complete'
+		print 'message:' +  "\nLogin to PHYLOViZ Online and access the tree at: " + outRoot + '/main/dataset/' + dataset['datasetID']
 
 
 def login(args, currentRoot): #Required before each of the tasks
@@ -58,23 +88,30 @@ def login(args, currentRoot): #Required before each of the tasks
 
 def checkDatasets(args, currentRoot): #Check if the database name to upload exists
 	print 'Checking if dataset name exists...'
+	if not args.t:
+		login(args, currentRoot)
+		bashCommand = 'curl --cookie jarfile -X GET '+currentRoot+'/api/db/postgres/find/datasets/name?name='+ args.d
+	else:
+		print args.t
+		#with open(cookie_file, 'w') as f:
+			#f.write(args.root+'\tTRUE\t/\tFALSE\t0\t' + args.t.split('=')[0] + '\t' + args.t.split('=')[1])
+		bashCommand = 'curl --cookie '+args.t+' -X GET '+currentRoot+'/api/db/postgres/find/datasets/name?name='+ args.d
 	
-	login(args, currentRoot)
-
-	bashCommand = 'curl --cookie jarfile -X GET '+currentRoot+'/api/db/postgres/find/datasets/name?name='+ args.d
 	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 	output = process.communicate()[0]
 	io = StringIO(output)
 	existingdatasets = json.load(io)
 
 	if len(existingdatasets['userdatasets']) > 0:
-		print 'dataset name already exists'
+		print 'message:' + 'dataset name already exists'
+		print 'code:exists'
 		sys.exit()
 
 def remoteUpload(args, currentRoot): #upload the input files to the database
 	print 'Uploading files...'
 
-	login(args, currentRoot)
+	if not args.t:
+		login(args, currentRoot)
 
 	addMetadata = ''
 
@@ -107,7 +144,17 @@ def remoteUpload(args, currentRoot): #upload the input files to the database
 	else:
 		makePublic = 'false'
 	
-	bashCommandUpload = 'curl --cookie jarfile \
+	if not args.t:
+		bashCommandUpload = 'curl --cookie jarfile \
+					  -F datasetName='+ datasetName +' \
+					  -F dataset_description='+ description +' \
+					  -F makePublic='+ makePublic +' \
+					  ' + dataToAdd + ' \
+					  ' + addMetadata + ' \
+					  -F numberOfFiles='+ str(numberOfFiles) +' \
+					  '+currentRoot+'/api/db/postgres/upload'
+	else:
+		bashCommandUpload = 'curl --cookie '+args.t+' \
 					  -F datasetName='+ datasetName +' \
 					  -F dataset_description='+ description +' \
 					  -F makePublic='+ makePublic +' \
@@ -118,37 +165,70 @@ def remoteUpload(args, currentRoot): #upload the input files to the database
 
 	process = subprocess.Popen(bashCommandUpload.split(), stdout=subprocess.PIPE)
 	output = json.loads(process.communicate()[0])
+
 	if "errorMessage" in output:
-		print output["errorMessage"]
+		print 'message:' + output["errorMessage"]
+		print 'code:uploaderror'
 		sys.exit()
 
 	return output
 
-def rungoeBURST(args, datasetID, currentRoot): #run the goeBURST algorithm to store the links in the database
+def rungoeBURST(args, datasetID, currentRoot, onqueue): #run the goeBURST algorithm to store the links in the database
+	
+	if args.mt:
+		missing_T = args.mt
+	else:
+		missing_T = 0
 
-	login(args, currentRoot)
+	if args.pid:
+		parent_id = args.pid
+	else:
+		parent_id = 'false'
 
-	print 'Running goeBURST...'
+	if not args.t:
+		login(args, currentRoot)
+		print 'Running goeBURST...'
+		if args.mc == False:
+			bashCommand = 'curl --cookie jarfile -X GET '+currentRoot+'/api/algorithms/goeBURST?dataset_id='+ datasetID + '&save=true&analysis_method=' + args.am + '&onqueue=' + onqueue + '&parent_id=' + parent_id
+		else:
+			bashCommand = 'curl --cookie jarfile -X GET '+currentRoot+'/api/algorithms/goeBURST?dataset_id='+ datasetID + '&save=true&missings=true&missingchar=' + str(args.mc) + '&analysis_method=' + args.am + '&missing_threshold='+str(missing_T)+'&onqueue=' + onqueue + '&parent_id=' + parent_id
+	else:
+		print 'Running goeBURST...'
+		print 'cookie'
+		if args.mc == False:
+			bashCommand = 'curl --cookie '+args.t+' -X GET '+currentRoot+'/api/algorithms/goeBURST?dataset_id='+ datasetID + '&save=true&analysis_method=' + args.am + '&onqueue=' + onqueue + '&parent_id=' + parent_id
+		else:
+			bashCommand = 'curl --cookie '+args.t+' -X GET '+currentRoot+'/api/algorithms/goeBURST?dataset_id='+ datasetID + '&save=true&missings=true&missingchar=' + str(args.mc) + '&analysis_method=' + args.am + '&missing_threshold='+str(missing_T)+'&onqueue=' + onqueue + '&parent_id=' + parent_id
+		
+		process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+		output = json.loads(process.communicate()[0])
 
-	bashCommand = 'curl --cookie jarfile -X GET '+currentRoot+'/api/algorithms/goeBURST?dataset_id='+ datasetID + '&save=true' + '&missingchar=' + args.missing_char +'&missings=' + args.missing_data
+	return output
 
-	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-	output = process.communicate()[0]
+	#print output
 
 
 def generatePublicLink(args, datasetID, currentRoot):
 
-	login(args, currentRoot)
 	print 'Generating Sharable Link...'
-	#make data set visible
-	bashCommand = 'curl --cookie jarfile -X PUT -d dataset_id='+ datasetID + ' -d change=true '+currentRoot+'/api/db/postgres/update/all/is_public'
 
+	if not args.t:
+		login(args, currentRoot)
+		#make data set visible	
+		bashCommand = 'curl --cookie jarfile -X PUT -d dataset_id='+ datasetID + ' -d change=true '+currentRoot+'/api/db/postgres/update/all/is_public'
+	else:
+		bashCommand = 'curl --cookie '+args.t+' -X PUT -d dataset_id='+ datasetID + ' -d change=true '+currentRoot+'/api/db/postgres/update/all/is_public'
+	
 	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 	output = process.communicate()[0]
 
-	login(args, currentRoot)
-	#get sharable link
-	bashCommand = 'curl --cookie jarfile -X GET '+currentRoot+'/api/utils/publiclink?dataset_id='+ datasetID
+	if not args.t:
+		login(args, currentRoot)
+		#get sharable link
+		bashCommand = 'curl --cookie jarfile -X GET '+currentRoot+'/api/utils/publiclink?dataset_id='+ datasetID
+	else:
+		bashCommand = 'curl --cookie '+args.t+' -X GET '+currentRoot+'/api/utils/publiclink?dataset_id='+ datasetID
+	
 	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 	output = json.loads(process.communicate()[0])
 	
--- job_processing/phyloviz_functions.py
+++ job_processing/phyloviz_functions.py
@@ -11,7 +11,7 @@ import datetime
 import fast_mlst_functions
 import database_functions
 
-from config import wg_index_correspondece, core_index_correspondece, core_headers_correspondece, wg_headers_correspondece, allele_classes_to_ignore
+from config import wg_index_correspondece, core_index_correspondece, core_headers_correspondece, wg_headers_correspondece, allele_classes_to_ignore, BASE_ROOT
 
 database_correspondece = {"E.coli":Ecoli}
 
@@ -220,8 +220,10 @@ def send_to_phyloviz(job_ids, dataset_name, dataset_description, additional_data
 	else:
 		missing_data_to_use = "false"
 
+	#command = 'python ./app/resources/phyloviz/remoteUpload.py -t '+cookie_string+' -cd '+config.final_root+' -am '+analysis_method+' -root '+config.final_root+' -mc ' + req.body.missingschar + ' -mt ' + missing_threshold + ' -sdt profile -sd uploads/'+profileName+' -d "'+req.body.name+'" -dn "'+req.body.description+'" -m uploads/'+metadataName+' -pid ' + parent_id;
 
-	command = 'python ./app/resources/phyloviz/remoteUpload.py -u innuendo_demo -p innuendo_demo -sdt profile -sd ' + file_path_profile + ' -m '+ file_path_metadata +' -d ' + dataset_name + ' -dn ' + dataset_description + '-l' + ' --missing_data ' + missing_data_to_use + ' --missing_char ' + missing_char;
+	command = 'python ./app/resources/phyloviz/remoteUpload.py -cd '+BASE_ROOT+' -am core -root '+BASE_ROOT+' -mc ' + missing_char + ' -mt 0 -sdt profile -sd ' + file_path_profile + ' -d "'+dataset_name+'" -dn "'+dataset_description+'" -m '+ file_path_metadata +' -pid ' + parent_id;
+	#command = 'python ./app/resources/phyloviz/remoteUpload.py -u innuendo_demo -p innuendo_demo -sdt profile -sd ' + file_path_profile + ' -m '+ file_path_metadata +' -d ' + dataset_name + ' -dn ' + dataset_description + '-l' + ' --missing_data ' + missing_data_to_use + ' --missing_char ' + missing_char;
 	#command = 'python ./app/resources/phyloviz/remoteUpload.py -u innuendo_demo -p innuendo_demo -sdt profile -sd ' + file_path_profile + ' -d ' + args.dataset_name + ' -dn ' + args.dataset_description + '-l';
 	command = command.split(' ')
 

233717265e7d7d37e446141229a88840d4da01cc
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-07-07 10:38:10 +0100

    fix modal problematic jobs bug

--- app/static/controllers/js_objects/single_project.js
+++ app/static/controllers/js_objects/single_project.js
@@ -1327,7 +1327,7 @@ function Single_Project(CURRENT_PROJECT_ID, CURRENT_PROJECT, $http, $rootScope){
 		        								//console.log(strainID_pipeline, strain_name);
 		        								for(tk in response.data.tasks){
 		        									dict_of_tasks_status[response.data.tasks[tk]] = '';
-		        									//if(response.data.tasks[tk].indexOf('null') < 0) periodic_check_job_status(response.data.tasks[tk], dict_of_tasks_status, strain_names[strain_name], response.data.tasks[tk], strainID_pipeline[strains_dict[strain_names[strain_name]]], CURRENT_PROJECT_ID);
+		        									if(response.data.tasks[tk].indexOf('null') < 0) periodic_check_job_status(response.data.tasks[tk], dict_of_tasks_status, strain_names[strain_name], response.data.tasks[tk], strainID_pipeline[strains_dict[strain_names[strain_name]]], CURRENT_PROJECT_ID);
 		        								}
 		        							})
 					        			})
@@ -1396,7 +1396,7 @@ function Single_Project(CURRENT_PROJECT_ID, CURRENT_PROJECT, $http, $rootScope){
 								dict_of_tasks_status[t_id] = '';
 								//console.log(strain_id, pip_id);
 								//console.log(buttons_to_tasks);
-								periodic_check_job_status(t_id, dict_of_tasks_status, strain_id, pr_ids[l], pip_id, proj_id);
+								//periodic_check_job_status(t_id, dict_of_tasks_status, strain_id, pr_ids[l], pip_id, proj_id);
 							}
 						}
 						countstrains += 1;

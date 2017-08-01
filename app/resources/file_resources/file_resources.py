07b8cc7f860b76be543308d9b68befb04fb15d56
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-08-01 11:31:55 +0100

    strain batch submission template

--- app/resources/file_resources/file_resources.py
+++ app/resources/file_resources/file_resources.py
@@ -1,7 +1,7 @@
 from app import app, db
-from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with, send_from_directory #filters data according to some fields
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with #filters data according to some fields
 from flask_security import current_user
-from flask import jsonify
+from flask import jsonify, send_from_directory
 
 from flask_security import current_user, login_required, roles_required, auth_token_required
 import datetime

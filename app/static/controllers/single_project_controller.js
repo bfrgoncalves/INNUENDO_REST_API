045fbe948b87bfdc5965ce48312a8fb70d1f9b16
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-08-01 14:39:52 +0100

    add option to change column visibility

--- app/static/controllers/single_project_controller.js
+++ app/static/controllers/single_project_controller.js
@@ -261,7 +261,7 @@ innuendoApp.controller("projectCtrl", function($scope, $rootScope, $http) {
 
 						        console.log(p_col_defs);
 
-						        $scope.strains_headers = strains_headers;					      
+						        //$scope.strains_headers = strains_headers;					      
 
 						        console.log($scope.strains_headers);
 

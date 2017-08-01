544e52ee2c385f082673df0d0a135e9facb1afce
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-08-01 11:40:16 +0100

    strain batch submission template

--- app/resources/file_resources/file_resources.py
+++ app/resources/file_resources/file_resources.py
@@ -1,10 +1,11 @@
 from app import app, db
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with #filters data according to some fields
 from flask_security import current_user
-from flask import jsonify, send_from_directory
+from flask import jsonify, send_file
 
 from flask_security import current_user, login_required, roles_required, auth_token_required
 import datetime
+import os
 
 '''
 File resources:
@@ -16,4 +17,14 @@ class TemplateResource(Resource):
 	@login_required
 	def get(self):
 		templates_folder = 'app/static/file_templates'
-		return send_from_directory(directory=templates_folder, filename="template_strain_batch_submission.tab")
+		file_path = os.path.join(templates_folder, "template_strain_batch_submission.tab")
+		print file_path
+		try:
+			response = send_file(file_path, as_attachment=True)
+			response.headers.add('Access-Control-Allow-Origin', '*')
+			response.headers.add('Content-Type', 'application/force-download')
+			return response
+		except Exception as e:
+			print e
+			#self.Error(400)
+			return 404

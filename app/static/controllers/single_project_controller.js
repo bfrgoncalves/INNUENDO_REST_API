3f7af34d20e42b5006b680c265b1c9bb0524d991
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-08-01 15:01:35 +0100

    add option to change column visibility

--- app/static/controllers/single_project_controller.js
+++ app/static/controllers/single_project_controller.js
@@ -536,7 +536,7 @@ innuendoApp.controller("projectCtrl", function($scope, $rootScope, $http, $timeo
 
 		single_project.get_project_strains(function(strains_results){
 			global_strains = strains_results.strains;
-			objects_utils.loadDataTables('strains_table', global_strains, project_col_defs, strains_headers);
+			//objects_utils.loadDataTables('strains_table', global_strains, project_col_defs, strains_headers);
 			callback();
 		});
 	}

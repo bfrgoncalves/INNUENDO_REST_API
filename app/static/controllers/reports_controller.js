381b840b34f7bfac69b7d01b095daa95cdd68ee0
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-08-02 15:42:29 +0100

    add option to change column visibility

--- app/static/controllers/reports_controller.js
+++ app/static/controllers/reports_controller.js
@@ -1286,6 +1286,8 @@ innuendoApp.controller("reportsCtrl", function($scope, $rootScope, $http) {
 
 							restore_table_headers('reports_info_table', reports_info_table_headers, function(){
 								restore_table_headers('reports_results_table', reports_results_table_headers, function(){
+									objects_utils.destroyTable('reports_info_table');
+									objects_utils.destroyTable('reports_results_table');
 									//objects_utils.loadDataTables('reports_info_table', run_infos, headers_defs_info[0], reports_info_table_headers);
 									//objects_utils.loadDataTables('reports_results_table', run_results, headers_defs_results[0], reports_results_table_headers);
 									modal_alert_message = 'Reports added to the project.';

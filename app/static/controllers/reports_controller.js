7987529420c0379c4349ca0dd515ca2ce7e1db65
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-08-02 15:02:39 +0100

    add option to change column visibility

--- app/static/controllers/reports_controller.js
+++ app/static/controllers/reports_controller.js
@@ -1269,41 +1269,35 @@ innuendoApp.controller("reportsCtrl", function($scope, $rootScope, $http) {
 							headers_defs_info = set_headers(run_infos);
 							headers_defs_results = set_headers(run_results);
 							
-							$scope.$apply(function(){
-								$scope.reports_info_table_headers = headers_defs_info[1];
-								$scope.reports_results_table_headers = headers_defs_results[1];
-								console.log($scope.reports_results_table_headers, $scope.reports_info_table_headers);
-							})	
-
 							reports_info_table_headers = headers_defs_info[1];
 							reports_results_table_headers = headers_defs_results[1];
+
+							restore_table_headers('reports_info_table', reports_info_table_headers);
+							restore_table_headers('reports_results_table', reports_results_table_headers);
 							
-							setTimeout(function(){
-								objects_utils.loadDataTables('reports_info_table', run_infos, headers_defs_info[0], reports_info_table_headers);
-								console.log("DONE");
-								objects_utils.loadDataTables('reports_results_table', run_results, headers_defs_results[0], reports_results_table_headers);
+							objects_utils.loadDataTables('reports_info_table', run_infos, headers_defs_info[0], reports_info_table_headers);
+							objects_utils.loadDataTables('reports_results_table', run_results, headers_defs_results[0], reports_results_table_headers);
 
-								modal_alert_message = 'Reports added to the project.';
-								if(problematic_jobs.length > 0){
-									modal_alert_message += '\nCould not load some projects. There seems to a be a problem with them. Job ids: ';
-									p_jobs = ""
-									for(pj in problematic_jobs){
-										p_jobs += problematic_jobs[pj] + ", ";
-									}
-									modal_alert_message += p_jobs
-									modal_alert_message = modal_alert_message.substr(0, modal_alert_message.length-1);
-									modal_alert_message += modal_alert_message + "\n Try do re-do the analysis for the procedures with those job ids."
+							modal_alert_message = 'Reports added to the project.';
+							if(problematic_jobs.length > 0){
+								modal_alert_message += '\nCould not load some projects. There seems to a be a problem with them. Job ids: ';
+								p_jobs = ""
+								for(pj in problematic_jobs){
+									p_jobs += problematic_jobs[pj] + ", ";
 								}
-								modalAlert(modal_alert_message, function(){});
-								//objects_utils.show_message('s_report_message_div', 'success', 'Reports added to the project.')
-								$('#reports_info_table_wrapper').css({'display':'block'});
-								$('#reports_results_table_wrapper').css({'display':'none'});
-								$('#reports_metadata_table_wrapper').css({'display':'none'});
+								modal_alert_message += p_jobs
+								modal_alert_message = modal_alert_message.substr(0, modal_alert_message.length-1);
+								modal_alert_message += modal_alert_message + "\n Try do re-do the analysis for the procedures with those job ids."
+							}
+							modalAlert(modal_alert_message, function(){});
+							//objects_utils.show_message('s_report_message_div', 'success', 'Reports added to the project.')
+							$('#reports_info_table_wrapper').css({'display':'block'});
+							$('#reports_results_table_wrapper').css({'display':'none'});
+							$('#reports_metadata_table_wrapper').css({'display':'none'});
 
-								//$("#run_info_" + q[p]).trigger("click");
+							//$("#run_info_" + q[p]).trigger("click");
 
-								callback(true);
-							},1000);
+							callback(true);
 						
 						}, 500);
 					}

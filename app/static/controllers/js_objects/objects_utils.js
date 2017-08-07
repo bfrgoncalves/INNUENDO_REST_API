b4e0e4b497e792c4f78e2331f6dba3a417877690
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-08-07 14:23:06 +0100

    persistent row selection

--- app/static/controllers/js_objects/objects_utils.js
+++ app/static/controllers/js_objects/objects_utils.js
@@ -143,7 +143,7 @@ function Objects_Utils(){
 				    for(j in CURRENT_TABLE_ROW_ANALYSIS_SELECTED[table_id]){
 				    	if($.inArray(CURRENT_TABLE_ROW_ANALYSIS_SELECTED[table_id][j], already_added) == -1){
 				    		console.log("AQUI", CURRENT_TABLE_ROW_ANALYSIS_SELECTED[table_id][j], $('#'+table_id+' tbody tr').eq(CURRENT_TABLE_ROW_ANALYSIS_SELECTED[table_id][j] + count_clicked + 1), CURRENT_TABLE_ROW_ANALYSIS_SELECTED[table_id][j] + count_clicked);
-				    		$('#'+table_id+' tbody').find("tr:eq("+String(CURRENT_TABLE_ROW_ANALYSIS_SELECTED[table_id][j] + count_clicked)+") td button.analysis-control").trigger("click");
+				    		$('#'+table_id+' tbody').find("tr:eq("+String(CURRENT_TABLE_ROW_ANALYSIS_SELECTED[table_id][j] + count_clicked + 1)+") td button.analysis-control").trigger("click");
 				    		count_clicked += 1;
 				    	}
 				    }

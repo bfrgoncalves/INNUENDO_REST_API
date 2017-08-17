d36c38e94447c95bad0c281744b3657deb9ec812
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-08-17 11:22:59 +0100

    fix table column order when adding batch file

--- app/static/controllers/single_project_controller.js
+++ app/static/controllers/single_project_controller.js
@@ -90,8 +90,8 @@ function set_headers_single_project(global_strains){
 		
 		for(p in headers_order){
 			for(x in global_strains[0]){
-				console.log(x, p, dict_fields[p], dict_fields);
-				if(x == dict_fields[p]){
+				console.log(x, headers_order[p], dict_fields[headers_order[p]], dict_fields);
+				if(x == dict_fields[headers_order[p]]){
 					console.log("AQUI");
 					if (x != "Analysis" && x != "id" && x != "species_id" && x != "lab_protocols"){
 						if($.inArray(matching_fields[x], minimal_fields) > -1){

59b7eaa77814f1b7bb479df4f55c0516a1518c31
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-11-08 22:22:54 +0000

    protocol buttons

--- app/static/controllers/js_objects/objects_utils.js
+++ app/static/controllers/js_objects/objects_utils.js
@@ -294,7 +294,7 @@ function Objects_Utils(){
 	        	console.log(isShift);
 
 	        	if(isShift){
-	        		$("#"+strainID+"_"+workflow_name).trigger("change");
+	        		$("#"+strainID+"_"+workflow_name).find('[data-toggle=dropdown]').dropdown('toggle');
 	        	}
 
 	        }
@@ -308,7 +308,7 @@ function Objects_Utils(){
 	        	var strainID = $(this).attr('strainID');
 	        	var shown = $(this).attr("shown_child");
 
-	        	$("#"+strainID+"_"+workflow_name).trigger("change");
+	        	$("#"+strainID+"_"+workflow_name).find('[data-toggle=dropdown]').dropdown('toggle');
 	        	
 
 	        }

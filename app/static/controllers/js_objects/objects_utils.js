f591cbdb24a4854dba7682edee36afea6be95ff2
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-11-08 20:58:10 +0000

    protocol buttons

--- app/static/controllers/js_objects/objects_utils.js
+++ app/static/controllers/js_objects/objects_utils.js
@@ -158,7 +158,7 @@ function Objects_Utils(){
 				    	}
 				    }
 
-				    //$('#'+table_id+' tbody').find("tr.selected td button.analysis-control").trigger("click");
+				    $('#'+table_id+' tbody').find("tr.selected td button.analysis-control").trigger("click");
 				    //$('#'+table_id+' tbody').find("tr td button.button_table_to_trigger").trigger("click");
 				    $('.child_row').css({"background-color":"#eeffff"});
 
@@ -256,7 +256,7 @@ function Objects_Utils(){
 	        		$("#"+prevWorkflow[0]+"_workflows").css({"display":"none"});
 	        		$(prevWorkflow[2]).attr("shown_child", "false");
 	        	}
-	        	
+
         		if(shown =='false'){
 	        		console.log("entrou")
 	        		$("#"+strainID+"_protocols").empty();

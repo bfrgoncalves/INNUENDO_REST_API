d3328e700e5badfa6c6bbc899fd59215240e1617
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-11-08 20:45:21 +0000

    protocol buttons

--- app/static/controllers/js_objects/objects_utils.js
+++ app/static/controllers/js_objects/objects_utils.js
@@ -158,8 +158,8 @@ function Objects_Utils(){
 				    	}
 				    }
 
-				    $('#'+table_id+' tbody').find("tr.selected td button.analysis-control").trigger("click");
-				    $('#'+table_id+' tbody').find("tr td button.button_table_to_trigger").trigger("click");
+				    //$('#'+table_id+' tbody').find("tr.selected td button.analysis-control").trigger("click");
+				    //$('#'+table_id+' tbody').find("tr td button.button_table_to_trigger").trigger("click");
 				    $('.child_row').css({"background-color":"#eeffff"});
 
             }

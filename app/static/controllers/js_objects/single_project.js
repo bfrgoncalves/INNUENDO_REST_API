1af1c289f7ad739b33f0c9c2c16c4374eeeebea0
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-11-10 15:46:49 +0000

    set job function to use nextflow

--- app/static/controllers/js_objects/single_project.js
+++ app/static/controllers/js_objects/single_project.js
@@ -1398,7 +1398,7 @@ function Single_Project(CURRENT_PROJECT_ID, CURRENT_PROJECT, $http, $rootScope){
 
 
 					        				//Add job id to the process on ngsonto and start checking the job status
-					        				ngs_onto_requests.ngs_onto_request_add_jobid_to_process(strainID_pipeline[strains_dict[strain_names[strain_name]]], processes_to_map, task_ids_to_map, strain_name, function(response, strain_name){
+					        				/*ngs_onto_requests.ngs_onto_request_add_jobid_to_process(strainID_pipeline[strains_dict[strain_names[strain_name]]], processes_to_map, task_ids_to_map, strain_name, function(response, strain_name){
 		        								count_strains_added_run += 1;
 
 		        								for(tk in response.data.tasks){
@@ -1411,7 +1411,7 @@ function Single_Project(CURRENT_PROJECT_ID, CURRENT_PROJECT, $http, $rootScope){
 		        									modalAlert("Jobs for all the selected strains have been submitted", function(){});
 		        									$('#button_run_strain').fadeTo("slow", 1).css('pointer-events','auto');
 		        								}
-		        							})
+		        							})*/
 					        			})
 			        				}
 			        				

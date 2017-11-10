f194755d92191661e349ad8d2d8f05989384359b
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-11-10 16:05:18 +0000

    set job function to use nextflow

--- app/static/controllers/js_objects/single_project.js
+++ app/static/controllers/js_objects/single_project.js
@@ -1058,16 +1058,17 @@ function Single_Project(CURRENT_PROJECT_ID, CURRENT_PROJECT, $http, $rootScope){
 			        		pipelines_type_by_strain[strain_data[counter]['strainID']][1].push(buttonselectedPipeline.replace("&&&", ""));
 			        		pipelines_type_by_strain[strain_data[counter]['strainID']][2].push(protocol_buttons.replace("&&&", ""));
 			        	}
-
-
-				        for(j in pipelines_type_by_strain[strain_data[counter]['strainID']]){
-				        	for(o in pipelines_type_by_strain[strain_data[counter]['strainID']][j]){
-				        		if(j == 0) toAdd_lab_protocols += pipelines_type_by_strain[strain_data[counter]['strainID']][j][o];
-		        				else if (j==1) toAdd_analysis += pipelines_type_by_strain[strain_data[counter]['strainID']][j][o];
-				        		else toAdd_Protocols = pipelines_type_by_strain[strain_data[counter]['strainID']][j][o];
-				        	}
-				        }
 			        }
+
+
+			        for(j in pipelines_type_by_strain[strain_data[counter]['strainID']]){
+			        	for(o in pipelines_type_by_strain[strain_data[counter]['strainID']][j]){
+			        		if(j == 0) toAdd_lab_protocols += pipelines_type_by_strain[strain_data[counter]['strainID']][j][o];
+	        				else if (j==1) toAdd_analysis += pipelines_type_by_strain[strain_data[counter]['strainID']][j][o];
+			        		else toAdd_Protocols = pipelines_type_by_strain[strain_data[counter]['strainID']][j][o];
+			        	}
+			        }
+			        
 			    }
 		        else if(mode=='same'){
 		        	for(j in pipelines_type_by_strain[strain_data[counter]['strainID']]){

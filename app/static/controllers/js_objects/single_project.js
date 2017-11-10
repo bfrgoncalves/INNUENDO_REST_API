6bf45c08d5c7b856ac45630881ded1fb7c186855
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-11-10 15:21:43 +0000

    set job function to use nextflow

--- app/static/controllers/js_objects/single_project.js
+++ app/static/controllers/js_objects/single_project.js
@@ -1314,6 +1314,7 @@ function Single_Project(CURRENT_PROJECT_ID, CURRENT_PROJECT, $http, $rootScope){
 			        		if(buttons_to_tasks[pi_name] == undefined){
 			        			dict_strain_names[strain_names[strain_name]][1].push(pipelines_applied[strain_names[strain_name]][p].split('button')[1].split('</i>')[1].split('<')[0]);
 			        			protocols_in_pip = protocols_applied_by_pipeline[strain_names[strain_name]][real_pi_name][0].split("</button>");
+			        			protocols_in_pip.pop();
 			        			for(protoc in protocols_in_pip){
 			        				console.log(protocols_in_pip);
 			        				protocol_name = protocols_in_pip[protoc].split("id=")[1].split('"')[1]

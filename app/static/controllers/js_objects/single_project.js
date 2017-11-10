f591efc030cd015a1f61d9c6a9dd5066cbed10df
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-11-10 16:30:47 +0000

    set job function to use nextflow

--- app/static/controllers/js_objects/single_project.js
+++ app/static/controllers/js_objects/single_project.js
@@ -1791,8 +1791,8 @@ function Single_Project(CURRENT_PROJECT_ID, CURRENT_PROJECT, $http, $rootScope){
 						delete current_job_status_color[sp_name];
 						delete tasks_to_buttons[buttons_to_tasks[sp_name]];
 						delete buttons_to_tasks[sp_name];
-						console.log(sp_name, removed_pip_name);
-						delete protocols_applied_by_pipeline[sp_name][removed_pip_name]
+						console.log(strain_names[index], removed_pip_name);
+						delete protocols_applied_by_pipeline[strain_names[index]][removed_pip_name]
 
 						strainNames_to_pipelinesNames[strain_names[index]].pop();
 						protocols_applied[strain_names[index]].pop();

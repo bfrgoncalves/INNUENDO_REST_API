065ad23c1307b06512d88a380ca807d75eb54fd9
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-11-14 18:23:02 +0000

    add color by status on workflow buttons

--- app/static/controllers/js_objects/single_project.js
+++ app/static/controllers/js_objects/single_project.js
@@ -369,7 +369,7 @@ function Single_Project(CURRENT_PROJECT_ID, CURRENT_PROJECT, $http, $rootScope){
 						}
 						else prev_process_status = status;
 
-						if (prev_workflow != process_id_to_workflow[counter_processes]){
+						if (prev_workflow != process_id_to_workflow[counter_processes] || response.data.length - 1 == n){
 
 							protocols_on_button[process_id_to_workflow[counter_processes]] = protocols_on_workflow;
 

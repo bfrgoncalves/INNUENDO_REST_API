8eeb649826859e75174671b505d427397626d4d2
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-11-15 14:08:00 +0000

    fix workflow removal case not run

--- app/static/controllers/js_objects/single_project.js
+++ app/static/controllers/js_objects/single_project.js
@@ -1879,7 +1879,7 @@ function Single_Project(CURRENT_PROJECT_ID, CURRENT_PROJECT, $http, $rootScope){
 						params = jobs_to_parameters[strainName_to_tids[strain_names[index]]];
 				        //pipeline_status[strainName_to_tids[strainID]](params[0], params[1], params[2], params[3]);
 				        console.log(params);
-				        if(params != undefined){
+				        if(params[0].length > 0){
 				        	params[0] = params[0].split(",");
 					        params[2] = params[2].split(",");
 

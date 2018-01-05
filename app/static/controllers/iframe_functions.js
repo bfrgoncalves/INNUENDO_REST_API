6403c64f928815eb6b55f16e09f5a191d1516451
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-01-05 13:18:14 +0000

    set iframe functions

--- app/static/controllers/iframe_functions.js
+++ app/static/controllers/iframe_functions.js
@@ -3,6 +3,8 @@ var setUpFrame = () => {
     const frame = window.frames['reportsIframe'];
     if(frame !== undefined){
     	console.log("tem frame");
+    	console.log(frame);
+    	console.log(frame.window);
 		frame.addUserData(current_user_name, current_user_id);
     }
 }

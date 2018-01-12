abefaf8aa2de45577a2c6167faf2c35272220939
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-01-12 15:19:09 +0000

    add logs modal

--- app/static/controllers/requests/requests.js
+++ app/static/controllers/requests/requests.js
@@ -849,7 +849,7 @@ function Requests(CURRENT_PROJECT_ID, CURRENT_PROJECT, $http) {
 
 		    $http(req).then(function(response){
 		    		console.log(response)
-				    callback();
+				    callback(response);
 		        },
 		        function(response){
 		            callback(response);

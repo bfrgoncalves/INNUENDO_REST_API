c554dd5bbd099831afed002277ea16ab1001f990
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-01-13 13:03:36 +0000

    get workdir

--- app/resources/jobs/jobs.py
+++ app/resources/jobs/jobs.py
@@ -274,7 +274,7 @@ class Job_queue(Resource):
 				elif "error" in jsonResult[0]["statusStr"]:
 					final_status = "FAILED"
 				
-				file2Path = jsonResult[0]["file2"].split("/")[-3]
+				file2Path = jsonResult[0]["file_2"].split("/")[-3]
 				print file2Path
 
 			except Exception as e:

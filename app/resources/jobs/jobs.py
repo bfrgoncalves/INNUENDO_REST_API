a9f2ec41469e448efdcd5785c141719c9c85fd4d
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-01-15 14:39:27 +0000

    get job folder

--- app/resources/jobs/jobs.py
+++ app/resources/jobs/jobs.py
@@ -257,7 +257,7 @@ class Job_queue(Resource):
 					final_status = "FAILED"
 				
 				try:
-					file2Path = jsonResult[0]["file_2"].split("/")[-3]
+					file2Path = '/'.join(jsonResult[0]["file_2"].split("/")[-3:-2])
 					print file2Path
 				except Exception as p:
 					file2Path = ""

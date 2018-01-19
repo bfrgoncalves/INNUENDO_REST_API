a7d8681eff128c81205da24b4b4679bcfa7d7f81
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-01-19 11:26:25 +0000

    check remove reports

--- app/static/controllers/js_objects/single_project.js
+++ app/static/controllers/js_objects/single_project.js
@@ -1294,6 +1294,9 @@ function Single_Project(CURRENT_PROJECT_ID, CURRENT_PROJECT, $http, $rootScope){
 
 		    	var sel_index = selected_indexes.shift();
 
+		    	$("#submission_status").empty();
+		        $("#submission_status").html("Saving pipeline " + String(count_finished+1) + " out of " + String(sel_index_clone.length) + "...");
+
 		    	create_pipeline(strains[sel_index].strainID, function(strain_id, pipeline_id){
 		        	strainID_pipeline[strain_id] = pipeline_id;
 		        	pipeline_ids.push(pipeline_id);

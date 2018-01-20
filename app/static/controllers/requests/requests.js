e982ad4291b22442e25ace1bb533e0e8cadfcfd0
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-01-20 15:53:05 +0000

    add results of seqtyping, pathopyping and chewbbaca status to metadata

--- app/static/controllers/requests/requests.js
+++ app/static/controllers/requests/requests.js
@@ -933,7 +933,7 @@ function Requests(CURRENT_PROJECT_ID, CURRENT_PROJECT, $http) {
 			
 		},
 
-		get_nextflow_log: function(filename, pipeline_id, project_id, submitter, callback){
+		get_nextflow_log: function(filename, pipeline_id, project_id, callback){
 			req = {
 		        //url: CURRENT_JOBS_ROOT + '/api/v1.0/jobs/results/download/',
 		        url: 'api/v1.0/jobs/logs/nextflow/',

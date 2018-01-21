d050852116ad2e2625c334266ea3a98a365f2c1c
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-01-21 02:34:19 +0000

    test reports integration

--- app/static/controllers/iframe_functions.js
+++ app/static/controllers/iframe_functions.js
@@ -1,6 +1,7 @@
 var setUpFrame = () => { 
     var frame = $("#reportsIframe").get(0).document || $("#reportsIframe").get(0).contentWindow;
     
+    console.log(frame);
     if(frame !== undefined){
 		frame.addUserData(current_user_name, current_user_id);
     }

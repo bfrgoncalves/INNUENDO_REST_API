ae459a2a29766e0684f4b7a6e1826888da296847
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-01-26 11:00:42 +0000

    test status

--- app/static/controllers/js_objects/single_project.js
+++ app/static/controllers/js_objects/single_project.js
@@ -364,8 +364,6 @@ function Single_Project(CURRENT_PROJECT_ID, CURRENT_PROJECT, $http, $rootScope){
 						counter_processes += 1;
 						task_id = response.data.stdout[n][0];
 						status = response.data.stdout[n][1];
-
-						console.log(task_id, tasks_to_buttons, status_dict[status]);
 						
 						var res_pos = n;
 						if (task_id == "null") return;

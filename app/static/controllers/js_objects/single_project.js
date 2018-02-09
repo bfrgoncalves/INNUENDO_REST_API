9a5d31ef36a48e324d8f46acba3ba1cc08513f9d
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-02-09 13:46:19 +0000

    test submit job ES6

--- app/static/controllers/js_objects/single_project.js
+++ app/static/controllers/js_objects/single_project.js
@@ -1315,15 +1315,15 @@ let Single_Project = (CURRENT_PROJECT_ID, CURRENT_PROJECT, $http, $rootScope) =>
 			        	}
 			        }
 
-			        console.log(pipelines_applied, counter, strain_data);
+			        console.log(pipelines_applied, counter, strain_data, pipelines_type_by_strain);
 
 
 			        for(const j in pipelines_type_by_strain[strain_data[counter]['strainID']]){
 			        	for(const o in pipelines_type_by_strain[strain_data[counter]['strainID']][j]){
 
 			        	    console.log(j);
-			        		if(j === 0) toAdd_lab_protocols += pipelines_type_by_strain[strain_data[counter]['strainID']][j][o];
-	        				else if (j === 1) toAdd_analysis += pipelines_type_by_strain[strain_data[counter]['strainID']][j][o];
+			        		if(j === String(0)) toAdd_lab_protocols += pipelines_type_by_strain[strain_data[counter]['strainID']][j][o];
+	        				else if (j === String(1)) toAdd_analysis += pipelines_type_by_strain[strain_data[counter]['strainID']][j][o];
 			        		else toAdd_Protocols = pipelines_type_by_strain[strain_data[counter]['strainID']][j][o];
 			        		console.log(toAdd_analysis);
 			        	}

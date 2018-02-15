98a8efb8e0dff4a1c051cb84bd24c4b9c382627b
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-02-15 11:48:47 +0000

    add piechart to description

--- app/static/controllers/js_objects/description.js
+++ app/static/controllers/js_objects/description.js
@@ -1,14 +1,15 @@
 
 
-const loadGoogleChart = () => {
+function loadGoogleChart() {
   // Load google charts
 
     console.log("LOAD PIE");
     google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
 
     // Draw the chart and set the chart values
-    const drawChart = () => {
-      const data = google.visualization.arrayToDataTable([
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
       ['Task', 'Hours per Day'],
       ['Work', 8],
       ['Friends', 2],
@@ -18,13 +19,11 @@ const loadGoogleChart = () => {
       ['Sleep', 7]
     ]);
 
-    google.charts.setOnLoadCallback(drawChart);
-
     // Optional; add a title and set the width and height of the chart
-    const options = {'title':'My Average Day', 'width':400, 'height':300};
+    var options = {'title':'My Average Day', 'width':400, 'height':300};
 
     // Display the chart inside the <div> element with id="piechart"
-    const chart = new google.visualization.PieChart(document.getElementById('piechart'));
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
     chart.draw(data, options);
     };
 };

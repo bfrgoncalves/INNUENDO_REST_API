43931489a3acea245e206aa41893c7cbdb4f5ed4
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-02-15 12:22:29 +0000

    add user piechart

--- app/static/controllers/js_objects/description.js
+++ app/static/controllers/js_objects/description.js
@@ -1,7 +1,61 @@
 
 
 const loadGoogleChart = () => {
-    Highcharts.chart('piechart', {
+    Highcharts.chart('piechart_quota', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Browser market shares January, 2015 to May, 2015'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: [{
+                name: 'IE',
+                y: 56.33
+            }, {
+                name: 'Chrome',
+                y: 24.03,
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Firefox',
+                y: 10.38
+            }, {
+                name: 'Safari',
+                y: 4.77
+            }, {
+                name: 'Opera',
+                y: 0.91
+            }, {
+                name: 'Other',
+                y: 0.2
+            }]
+        }]
+    });
+
+
+    Highcharts.chart('piechart_user', {
         chart: {
             plotBackgroundColor: null,
             plotBorderWidth: null,

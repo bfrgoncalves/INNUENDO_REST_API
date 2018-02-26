e6250e63069343c780585143af20d6aefb95763f
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-02-26 15:44:58 +0000

    date-util

--- app/static/controllers/js_objects/single_project.js
+++ app/static/controllers/js_objects/single_project.js
@@ -160,10 +160,8 @@ let Single_Project = (CURRENT_PROJECT_ID, CURRENT_PROJECT, $http, $rootScope) =>
 	            let has_same_files = false;
 	            let message = "";
 	            let message_to_add = "";
-                console.log(response);
+
 	            for(const s in strains){
-	                console.log(response);
-	                console.log(strains[s], s);
 	            	let md = JSON.parse(response.data.strain_metadata);
 	            	if(md.File_1 === strains[s].File_1){
 	            		has_same_files = true;

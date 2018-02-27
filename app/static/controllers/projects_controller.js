8d818c94e9e64843f2598ce0a6f2a9b7e7848c05
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-02-27 16:38:30 +0000

    update table on lock project

--- app/static/controllers/projects_controller.js
+++ app/static/controllers/projects_controller.js
@@ -249,6 +249,7 @@ innuendoApp.controller("projectsCtrl", ($scope, $http) => {
     $scope.LockProject = () => {
         projects_table.lock_project(CURRENT_PROJECT_ID, (response) => {
             projects = response.projects;
+            console.log(projects);
             objects_utils.loadDataTables('projects_table', projects, project_col_defs, []);
         });
     }

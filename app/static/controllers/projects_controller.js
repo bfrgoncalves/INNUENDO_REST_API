b5c223f4857584ef3885fdc7443bf45d1ba37af4
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-02-28 11:34:19 +0000

    test lock

--- app/static/controllers/projects_controller.js
+++ app/static/controllers/projects_controller.js
@@ -241,6 +241,7 @@ innuendoApp.controller("projectsCtrl", ($scope, $http) => {
         }
         projects_table.load_project(table_id, CURRENT_PROJECT_ID, false, (results) => {
             CURRENT_PROJECT = results.project;
+            console.log(results.project);
             PROJECT_STATUS = results.project.is_removed;
             PREVIOUS_PAGE_ARRAY.push([current_scope_template, CURRENT_PROJECT_ID, CURRENT_JOB_MINE, CURRENT_PROJECT, CURRENT_SPECIES_ID, CURRENT_SPECIES_NAME, CURRENT_USER_NAME, CURRENT_JOBS_ROOT, CURRENT_JOB_ID, CURRENT_PROJECT_NAME_ID, CURRENT_TABLE_ROWS_SELECTED, CURRENT_TABLE_ROW_ANALYSIS_SELECTED, PROJECT_STATUS]);
             $scope.selectedTemplate.path = results.template;

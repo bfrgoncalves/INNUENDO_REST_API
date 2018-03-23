eed235723cd2883ccbc862292b7c82a4b4a59799
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-03-23 14:49:22 +0000

    load reports from the project

--- app/static/controllers/iframe_functions.js
+++ app/static/controllers/iframe_functions.js
@@ -3,12 +3,14 @@
  * Function for iframe comunication with the platform.
  * Needs to use var declaration to work.
  */
-var setUpFrame = () => {
+var setUpFrame = (callback) => {
     const reportFrameEl = $("#reportsIframe");
     var frame = reportFrameEl.get(0).document || reportFrameEl.get(0).contentWindow;
 
     if(frame !== undefined){
-        frame.addUserData(current_user_name, current_user_id);
+        frame.addUserData(current_user_name, current_user_id, () => {
+            callback();
+        });
     }
 };
 
@@ -21,18 +23,16 @@ var loadReport = (selectedRows, current_project_d, scope) => {
 
     scope.$apply( () => {
         scope.selectedTemplate.path = '/app/static/html_components/reports_view.html';
+        setTimeout(() => {
+            setUpFrame(() => {
+                const reportFrameEl = $("#reportsIframe");
+                var frame = reportFrameEl.get(0).document || reportFrameEl.get(0).contentWindow;
+
+                if(frame !== undefined){
+                    frame.loadReport(selectedRows, current_project_d);
+                }
+            });
+        }, 2000);
     });
-
-    setTimeout(() => {
-        const reportFrameEl = $("#reportsIframe");
-        var frame = reportFrameEl.get(0).document || reportFrameEl.get(0).contentWindow;
-
-        console.log(frame);
-        console.log(selectedRows, current_project_d);
-        if(frame !== undefined){
-            frame.loadReport(selectedRows, current_project_d);
-        }
-
-    }, 4000);
 
 };

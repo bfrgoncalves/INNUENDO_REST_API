969260a026c8750ae4ba40344aee20ca2b806566
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-04-03 14:23:55 +0100

    test option to delete strain fastq

--- app/static/controllers/js_objects/single_project.js
+++ app/static/controllers/js_objects/single_project.js
@@ -212,7 +212,8 @@ let Single_Project = (CURRENT_PROJECT_ID, CURRENT_PROJECT, $http, $rootScope) =>
 		                }
 		                sd["strainID"] = data.strainID;
 		                sd["FilesLocation"] = data.fq_location;
-		                console.log(sd);
+		                sd["has_files"] = data[i].has_files;
+
 		                if(!strains_dict.hasOwnProperty($.trim(data.strainID))){
 		                    strains_dict[$.trim(data.strainID)] = data.id;
 		                }
